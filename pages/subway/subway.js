27b9bd38d7a7a08216f768cfee789009455fa6fd
yjliu <1229931433@qq.com>
2023-06-21 10:13:28 +0800

    各个城市地铁图

--- pages/subway/subway.js
+++ pages/subway/subway.js
@@ -3,37 +3,23 @@ var amapFile = require('../../utils/amap-wx.js');
 const { Config } = require('../../utils/config.js');
 
 Page({
+  data: {
+    cityList: [],
+  },
   onLoad: function () {
-    const that = this;
-    wx.getLocation({
-      type: 'gcj02', // 坐标类型为国测局坐标系
-      success: function(res) {
-        const latitude = res.latitude; // 纬度
-        const longitude = res.longitude; // 经度
-        
-        // 在成功获取到位置后，调用获取地铁信息的函数
-        that.getSubwayMap(latitude, longitude);
+    const that = this
+    wx.request({
+      url: 'https://webapi.amap.com/subway/data/citylist.json',
+      data: {
+        uid: new Date().getTime()
       },
-      fail: function(err) {
-        console.log(err);
+      success ({ data }) {
+        that.setData({ cityList: data.citylist })
       }
     })
   },
-  getSubwayMap(latitude, longitude) {
-    const myMap = new amapFile.AMapWX({ key: Config.key });
-    
-    myMap.getPoiAround({
-      location: `${longitude},${latitude}`, // 经纬度格式为"经度,纬度"
-      keywords: '珠江新城', // 关键词为地铁站
-      success: function(data) {
-        const subwayStations = data.pois; // 获取地铁站点列表
-        
-        // 在这里处理地铁站点数据，比如展示地铁图或者显示附近的地铁站点列表等
-        console.log(subwayStations);
-      },
-      fail: function(info) {
-        console.log(info);
-      }
-    });
-  },
+  gotoSubway (e) {
+    const { adcode } = e.currentTarget.dataset;
+    wx.navigateTo({ url: `/pages/webview/webview?src=${encodeURIComponent('http://101.33.202.68/?adcode=' + adcode)}` });
+  }
 });
